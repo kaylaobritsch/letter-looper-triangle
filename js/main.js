6ff34b2fcf181b7249cd46778a9639bbd1412ce3
kaylaobritsch <kaylaobritsch@MacBook-Pro.local>
2017-05-05 15:03:33 -0400

    Done

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var textInput = prompt('Enter a letter:');
+var triangle = "";
+var i = 0;
+
+for (i = 0; i < 10; i++) {
+  var triangle = triangle + textInput;
+  document.write(triangle + '<br>');
+}
