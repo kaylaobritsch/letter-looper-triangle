55010f990d7a2ac66ff2b09280f8bea73ad0682c
kaylaobritsch <kaylaobritsch@MacBook-Pro.local>
2017-05-05 15:05:59 -0400

    Fixed for Markbot

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var textInput = prompt('Enter a letter:');
-var triangle = "";
+var triangle = '';
 var i = 0;
 
 for (i = 0; i < 10; i++) {
-  triangle = triangle + textInput;
+  triangle += textInput;
   document.write(triangle + '<br>');
 }
